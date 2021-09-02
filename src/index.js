e161a15e6a60972e49f44627e7069934035c7f24
Jeff Kinley <jkinley@mac.com>
2021-09-01 21:26:29 -0700

    switch to hash router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools';
 import App from './App';
